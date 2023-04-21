205e4601755638ea90cf54cf13ad9b0241409dac
Iryna Kaleniuk <iryna2001ka@gmail.com>
2023-04-21 23:02:19 +0300

    bugs fixed

--- javaScript/login.js
+++ javaScript/login.js
@@ -1,6 +1,15 @@
 import createLoginForm from "./functions/createLoginForm.js";
 
 window.addEventListener("load", (e) => {
-    const loginForm = createLoginForm();
-    document.querySelector("#body__wrap").append(loginForm);
+    const logInBtn = document.querySelector("#login-btn");
+    const logInLink = document.querySelector("#login-link");
+
+    const logIn = () => {
+        document.querySelector("#login-container").style.display = "none";
+        const loginForm = createLoginForm();
+        document.querySelector("#body__wrap").append(loginForm);
+    }
+
+    logInBtn.addEventListener("click", logIn);
+    logInLink.addEventListener("click", logIn);
 })
